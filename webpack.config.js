f931141790997037a7037aa596efff87dfd7e3e4
Rox Alberga <73292385+RoxAlberga@users.noreply.github.com>
2021-01-29 12:20:02 +0100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: ["./src/geo.js", "./src/input.js"],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js'
+    },
+    plugins: [
+        new Dotenv({
+          sample: './.env.default',
+          path: './.env'
+        }),
+      ],
+  };
