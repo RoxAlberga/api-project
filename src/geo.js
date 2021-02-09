6463d4aad0be335d9ffa2b5535e8216748d98917
Rox Alberga <rosaangela.alberga@gmail.com>
2021-02-09 11:05:05 +0100

    Modified

--- src/geo.js
+++ src/geo.js
@@ -1,15 +1,20 @@
+import _ from 'lodash'
+
+
+ //selezione elementi HTML da modificare
+ 
+ let citySearched = document.querySelector('.searched-city');
+ let aqIndicator = document.querySelector('.aqi-indicator');
+ let warning = document.querySelector('.warning');
+ let title = document.querySelector('.title');
+ const cityForm= document.querySelector('.search');
+ const cityValue = document.querySelector('.search input');
+
+ //costanti API
+ const key = process.env.API_KEY;
 
 window.addEventListener('load', () =>{
-    //Selezione elementi HTML da modificare
     
-    let città = document.querySelector('.città');
-    let aqIndicator = document.querySelector('.indicatore-aqi');
-    let avviso = document.querySelector('.avviso');
-    let titolo = document.querySelector('.titolo');
-   
-   
-    
-    //Geolocalizzazione
 
     let latitude;
     let longitude;
@@ -23,7 +28,7 @@ window.addEventListener('load', () =>{
 
             //Fetching dei dati utili (aqi e città)
             
-            const key = process.env.API_KEY;
+
             const api = `https://api.waqi.info/feed/geo:${latitude};${longitude}/?token=${key}`;
 
             fetch(api)
@@ -31,33 +36,80 @@ window.addEventListener('load', () =>{
                     return data.json();
                 })
                 .then(data =>{
-                    const {aqi, city} = data.data;
+                    const aqi = _.get(data, 'data.aqi', 0);
+                    const city= _.get(data, 'data.city') 
 
                     //Cambio paragrafi
 
-                    titolo.textContent = "L'indice di qualità dell'aria a"
-                    città.textContent = `${city.name}`;
+                    title.textContent = 'L\'indice di qualità dell\'aria a';
+                    citySearched.textContent = `${city.name}`;
                     aqIndicator.textContent = aqi;
-                    avviso.textContent = "";
+                    warning.textContent = '';
                     
 
                     //Modifica dei colori dello sfondo in base all'AQI
                     if(aqi < 51){
-                        document.body.className = "Good";
+                        document.body.className = 'Good';
                     }else if(aqi>50 && aqi<101){
-                        document.body.className = "Moderate";
+                        document.body.className = 'Moderate';
                     }else if(aqi>100 && aqi<151){
-                        document.body.className = "Sensitive";
+                        document.body.className = 'Sensitive';
                     }else if(aqi>150 && aqi<201){
-                        document.body.className = "Unhealthy";
+                        document.body.className = 'Unhealthy';
                     }else if(aqi>200 && aqi<301){
-                        document.body.className = "Very";
+                        document.body.className = 'Very';
                     }else{
-                        document.body.className = "Hazard";
+                        document.body.className = 'Hazard';
                     }
                     
-                });
+                })
         })
     }
 });
+
+cityForm.addEventListener('submit', (event)=> {
+
+    
+    event.preventDefault(); //evita che si riaggiorni la pagina
+    const city = cityValue.value.trim(); //prende il valore dell'input
+    cityForm.reset(); //resetta il campo input
+    const api = `https://api.waqi.info/feed/${city}/?token=${key}`;
+
+    fetch(api)
+        .then(data =>{
+            return data.json();
+        })
+        .then(data =>{
+            const aqi = _.get(data, 'data.aqi', 0);
+            
+
+           //Cambio paragrafi
+           title.textContent = 'L\'indice di qualità dell\'aria a';
+           citySearched.textContent = `${city}`;
+           aqIndicator.textContent = aqi;
+           warning.textContent = '';
+           
+
+           //Modifica dei colori dello sfondo in base all'AQI
+           if(aqi < 51){
+               document.body.className = 'Good';
+           }else if(aqi>50 && aqi<101){
+               document.body.className = 'Moderate';
+           }else if(aqi>100 && aqi<151){
+               document.body.className = 'Sensitive';
+           }else if(aqi>150 && aqi<201){
+               document.body.className = 'Unhealthy';
+           }else if(aqi>200 && aqi<301){
+               document.body.className = 'Very';
+           }else{
+               document.body.className = 'Hazard';
+           }
+          
+
+        })
+
+        
+                    
+   
+})
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: ["./src/geo.js", "./src/input.js"],
+    entry: "./src/geo.js",
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'main.js'
